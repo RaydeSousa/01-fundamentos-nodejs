3da5997ebba4bc89631f83feebbd3fc459249e21
RaydeSousa <raydesousa2018@gmail.com>
2024-08-06 21:13:08 -0300

    Estrutura da aplicação - Salvando usuários em memória (Headers)

--- src/server.js
+++ src/server.js
@@ -1,17 +1,27 @@
 import http from 'node:http'
 
+const users = []
+
 const server = http.createServer((req, res) => {
     const { method, url } = req
 
     if (method == 'GET' && url == '/users') {
-        return res.end('Listagem de usuários')
+        return res
+        .setHeader('Content-type', 'application/json')
+        .end(JSON.stringify(users))
     }
 
     if (method == 'POST' && url == '/users') {
+        users.push({
+            id: 1,
+            name: 'Ray de Sousa',
+            email: 'raydesousa2018@gmail.com',
+        })
+        
         return res.end('Criação de usuário')
     }
     
-    return res.end('Hello Ignite')
+    return res.end('Hello World')
 })
 
 server.listen(3333) 
